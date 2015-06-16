c31685776208a21a1cb6559dce664bd8fcfc0479
Kevin Oh <aflashyrhetoric@gmail.com>
2015-06-16 17:04:16 -0400

    Added CDN for animate.css, began animating some of the basic front page stuff and creating the section-two navigation list. Decided not to go with the icon-based menu since there are already icons in the thing. Mobile interface will do away with this and simply load the two-backup.html files.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,39 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
 
+$(document).ready(function(){
+
+  // Page Load Animations
+
+  $('img.header-image, i.fa-envelope-o, p.email, ul.nav-list').addClass('animated fadeIn');
+  $('p.description').addClass('animated fadeInDown');
+
+
+  // Navigation List
+  $('ul.services-list li a').click(function(){
+    // The clicked item
+    var clicked = $(this);
+    var currentID = $('div.active-service').attr('id');
+    console.log(currentID);
+    var clickedID = clicked.attr("id");
+
+    // Highlight proper link
+    $('a.active').removeClass('active');
+    clicked.addClass("active");
+
+
+    // Fade out SERVICE BRAND
+    $('div.active-service div.brand i, div.active-service div.brand h2').addClass('animated fadeOut');
+    // $('div.active-service').removeclass('active-service');
+    // Fade out SERVICE DESCRIPTION
+    $('div.active-service p.service-description').addClass('animated fadeOut');
+    $('div#' + currentID).removeClass('active-service');
+
+    // Load new brand
+    $('div#' + clickedID).addClass('active-service');
+
+  });
+});
 
 /* This script pulls from the WDKEVO Tumblr Blog and then populates SECTION-FOUR of the homepage with two of the most recent posts, potentially with the ability to browse different hashtags upon further deliberation of such a feature's relevance and effect on load time of the site. */
 
